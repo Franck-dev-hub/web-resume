9c72809d16cb3c4bf08701157f053a669c2e50de
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-13 20:17:40 +0200

    Update qrcode border

--- JS/dark_mode.js
+++ JS/dark_mode.js
@@ -2,14 +2,14 @@ function toggleThemeInit() {
   const toggleBtn = document.getElementById('toggle-theme');
   if (!toggleBtn) return;
 
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-});
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+  });
 
-if (localStorage.getItem('theme') === 'dark') {
-  document.body.classList.add('dark-mode');
-}
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
 }
 
 window.addEventListener('DOMContentLoaded', toggleThemeInit);
